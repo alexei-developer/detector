0da97652e8d38c729a6efbcf231c2ceb0acaaf25
alexei-developer <alexei.developer@gmail.com>
2016-11-03 16:46:01 +0300

    Add command line option for set video source

--- apps/detector/main.cpp
+++ apps/detector/main.cpp
@@ -4,6 +4,7 @@
 
 #include <QCoreApplication>
 #include <QStringList>
+#include <QCommandLineParser>
 
 #include "config.h"
 #include "core/core.h"
@@ -13,18 +14,42 @@
 
 int main(int argc, char *argv[])
 {
-  QCoreApplication a(argc, argv);
+  QCoreApplication app(argc, argv);
+  QCoreApplication::setApplicationName("Detector");
+  QCoreApplication::setApplicationVersion(DETECTOR_VERSION);
 
   LOG_INFO << "Detector version:" << DETECTOR_VERSION;
   LOG_INFO << "Qt version:" << qVersion();
 
-  LOG_DEBUG << "Arguments:" << a.arguments().join(" ").toStdString();
+  LOG_DEBUG << "Arguments:" << app.arguments().join(" ").toStdString();
+
+  QCommandLineParser parser;
+  parser.addVersionOption();
+  parser.addHelpOption();
+
+  parser.addPositionalArgument("source", QCoreApplication::translate("main", "Source for video capture."));
+  parser.process(app);
+
+  const QStringList sources = parser.positionalArguments();
+  if (sources.size() != 1) {
+    LOG_ERROR << "Need one source for video capture";
+    parser.showHelp(1);
+  }
 
   detect::DetectorFace detectorFace;
   try {
-    detect::Video video(0);
-    video.AddDetector(&detectorFace);
-    video.Start();
+    QString url = sources.first();
+    bool is_device = false;
+    int device = url.toInt(&is_device);
+
+    std::shared_ptr<detect::Video> video;
+    if (is_device)
+      video = std::make_shared<detect::Video>(device);
+    else
+      video = std::make_shared<detect::Video>(url.toStdString());
+
+    video->AddDetector(&detectorFace);
+    video->Start();
 
     LOG_INFO << "Press <enter> key for finish...";
     std::cin.get();
